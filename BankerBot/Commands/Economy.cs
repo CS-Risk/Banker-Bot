114943c3531d5e5f527ec64e6f72cd30f90f1fd1
Mike <EdianCurrinir@gmail.com>
2018-09-15 01:18:24 -0400

    Initial Check In

---
+++ BankerBot/Commands/Economy.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace BankerBot.Commands
+{
+	public class Economy : ModuleBase
+	{
+		private SheetsService _sheetsService;
+
+		public Economy(SheetsService sheets)
+		{
+			_sheetsService = sheets;
+		}
+
+
+		[Command("SpendGold")]
+		public async Task SpendGold(decimal gold, string note = "")
+		{
+			// Get User
+			var user = (IGuildUser)Context.Message.Author;
+
+			// Create record
+			List<IList<Object>> newRecords = new List<IList<Object>>();
+			newRecords.Add(createRow(user, gold: gold.ToString()));
+
+			// Update Sheet
+			SpreadsheetsResource.ValuesResource.AppendRequest request =
+				_sheetsService.Spreadsheets.Values.Append(new ValueRange() { Values = newRecords }, Helpers.SpreadsheetId, getNewRange());
+			request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+			request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+			var response = request.Execute();
+
+			// Reply in Discord
+			await ReplyAsync(getCharacterName(user) + " spent " + gold.ToString() + " gp. (" + note + ")");
+		}
+
+		[Command("GiveGold")]
+		public async Task GiveGold(SocketGuildUser recipient, decimal gold, string note = "")
+		{
+			await GiveGold(getCharacterName(recipient.Nickname), gold, note);
+		}
+
+		[Command("GiveGold")]
+		public async Task GiveGold(string recipient, decimal gold, string note = "")
+		{
+			// Get User
+			var user = (IGuildUser)Context.Message.Author;
+
+			// Create record
+			List<IList<Object>> newRecords = new List<IList<Object>>();
+
+			// Create disbursing record
+			newRecords.Add(createRow(user, gold: (gold * -1).ToString(), note: note));
+
+			// Create receiving record
+			newRecords.Add(createRow(user, charcterName: recipient, gold: gold.ToString(), note: note));
+
+			// Update Sheet
+			SpreadsheetsResource.ValuesResource.AppendRequest request =
+				_sheetsService.Spreadsheets.Values.Append(new ValueRange() { Values = newRecords }, Helpers.SpreadsheetId, getNewRange());
+			request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+			request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+			var response = request.ExecuteAsync();
+
+			// Reply in Discord
+			await ReplyAsync(string.Format("{0} gave {1} {2} gp. {3}", getCharacterName(user), recipient, gold.ToString(), (!string.IsNullOrEmpty(note) ? string.Format("({0})", note) : "")));
+		}
+
+		[Command("UpdateGold")]
+		public async Task UpdateGold(SocketGuildUser character, decimal gold, string note = "")
+		{
+			await UpdateGold(getCharacterName(character.Nickname), gold, note);
+		}
+
+		[Command("UpdateGold")]
+		public async Task UpdateGold(string character, decimal gold, string note = "")
+		{
+			// Get User
+			var user = (IGuildUser)Context.Message.Author;
+			var dmRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == "DM");
+			if (!user.RoleIds.Contains(dmRole.Id))
+			{
+				throw new Exception("You do not have permission to do this.");
+			}
+
+			// Create record
+			List<IList<Object>> newRecords = new List<IList<Object>>();
+			newRecords.Add(createRow(user, charcterName: character, gold: gold.ToString(), note: note));
+
+			// Update Sheet
+			SpreadsheetsResource.ValuesResource.AppendRequest request =
+				_sheetsService.Spreadsheets.Values.Append(new ValueRange() { Values = newRecords }, Helpers.SpreadsheetId, getNewRange());
+			request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.OVERWRITE;
+			request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+			var response = request.ExecuteAsync();
+
+			// Reply in Discord
+			await ReplyAsync(string.Format("{0}'s gold value changed by {1}. {2}", character, gold.ToString(), (!string.IsNullOrEmpty(note) ? string.Format("({0})", note) : "")));
+		}
+
+		#region Helpers
+
+		private IList<Object> createRow(IGuildUser user, string charcterName = "", string tier = "", string checkpoints = "", string lootpoints = "", string gold = "", string note = "", string resurected = "")
+		{
+
+			IList<Object> obj = new List<Object>();
+
+			if (!string.IsNullOrWhiteSpace(charcterName)) // Character
+				obj.Add(charcterName);
+			else
+				obj.Add(getCharacterName(user));
+
+			obj.Add("BankerBot (" + user.Username + ")"); // DM
+			obj.Add(DateTime.Today.ToShortDateString()); // Date
+			obj.Add(tier); // Tier
+			obj.Add(checkpoints); // Checkpoints
+			obj.Add(lootpoints); // Lootpoints
+			obj.Add(gold); // Gold
+			obj.Add(note); // Notes
+			obj.Add(resurected); // Resurected
+
+			return obj;
+		}
+
+		private string getCharacterName(IGuildUser user)
+		{
+			return getCharacterName(user.Nickname);
+		}
+
+		private string getCharacterName(string nickname)
+		{
+			return Regex.Match(nickname, @"\(([^)]*)\)").Groups[1].Value;
+		}
+
+		private string getNewRange()
+		{
+			// Define request parameters.
+
+			SpreadsheetsResource.ValuesResource.GetRequest getRequest =
+				_sheetsService.Spreadsheets.Values.Get(Helpers.SpreadsheetId, Helpers.LogBookRange);
+
+			ValueRange getResponse = getRequest.Execute();
+			IList<IList<Object>> getValues = getResponse.Values;
+
+			if (getValues == null) return Helpers.LogBookRange;
+
+			// Get new range
+			int currentCount = getValues.Count() + 1;
+			String newRange = "'Logbook'!A" + currentCount + ":I";
+
+			return newRange;
+		}
+
+		#endregion
+	}
+}
---
+++ BankerBot/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace RPGBot
+{
+	class Program
+	{
+		private DiscordSocketClient _client;
+		private readonly CommandService _commands = new CommandService();
+		private readonly IServiceCollection _map = new ServiceCollection();
+		private IServiceProvider _services;
+		private string _token = "NDkwMTc1OTI0MjI3Mjc2ODAx.Dn1frg.d0eoR5GvOgqku778imX-AqF823g";
+
+		static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+		static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+		static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		private Program()
+		{
+
+
+			//Discord API
+			_client = new DiscordSocketClient();
+			_client.Log += Logger;
+			_commands.Log += Logger;
+		}
+
+		public async Task MainAsync()
+		{
+			// Centralize the logic for commands into a seperate method.
+			await InitCommands();
+
+			// Login and connect           
+			await _client.LoginAsync(TokenType.Bot, _token);
+			await _client.StartAsync();
+
+			// Wait infinitely so your bot actually stays connected.
+			await Task.Delay(-1);
+		}
+
+		private async Task InitCommands()
+		{
+			// Google Sheets Credentials
+			UserCredential credential;
+
+			using (var stream =
+				new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+			{
+				string credPath = "token.json";
+				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					Scopes,
+					"user",
+					CancellationToken.None,
+					new FileDataStore(credPath, true)).Result;
+				Console.WriteLine("Credential file saved to: " + credPath);
+			}
+
+			// Create Google Sheets API service.
+			var sheetsService = new SheetsService(new BaseClientService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = ApplicationName,
+			});
+
+			// Repeat this for all the service classes and other dependencies that your commands might need.
+			//_map.AddSingleton());
+
+			_map.AddSingleton(sheetsService);
+
+			// When all your required services are in the collection, build the container.
+			// Tip: There's an overload taking in a 'validateScopes' bool to make sure
+			// you haven't made any mistakes in your dependency graph.
+			_services = _map.BuildServiceProvider();
+
+
+			// Either search the program and add all Module classes that can be found.
+			// Module classes *must* be marked 'public' or they will be ignored.
+			await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+			// Subscribe a handler to see if a message invokes a command.
+			_client.MessageReceived += HandleCommandAsync;
+		}
+
+		private async Task HandleCommandAsync(SocketMessage arg)
+		{
+			// Bail out if it's a System Message.
+			var msg = arg as SocketUserMessage;
+			if (msg == null) return;
+
+			// Create a number to track where the prefix ends and the command begins
+			int pos = 0;
+			// Replace the '!' with whatever character
+			// you want to prefix your commands with.
+			if (msg.HasCharPrefix('?', ref pos))
+			{
+				// Create a Command Context.
+				var context = new SocketCommandContext(_client, msg);
+
+				// Execute the command. (result does not indicate a return value, rather an object stating if the command executed succesfully).
+				var result = await _commands.ExecuteAsync(context, pos, _services);
+
+				// Uncomment the following lines if you want the bot
+				// to send a message if it failed (not advised for most situations).
+				if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+					await msg.Channel.SendMessageAsync(result.ErrorReason);
+			}
+		}
+
+		// Example of a logging handler. This can be re-used by addons
+		// that ask for a Func<LogMessage, Task>.
+		private static Task Logger(LogMessage message)
+		{
+			var cc = Console.ForegroundColor;
+			switch (message.Severity)
+			{
+				case LogSeverity.Critical:
+				case LogSeverity.Error:
+					Console.ForegroundColor = ConsoleColor.Red;
+					break;
+				case LogSeverity.Warning:
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					break;
+				case LogSeverity.Info:
+					Console.ForegroundColor = ConsoleColor.White;
+					break;
+				case LogSeverity.Verbose:
+				case LogSeverity.Debug:
+					Console.ForegroundColor = ConsoleColor.DarkGray;
+					break;
+			}
+			Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message}");
+			Console.ForegroundColor = cc;
+
+			// If you get an error saying 'CompletedTask' doesn't exist,
+			// your project is targeting .NET 4.5.2 or lower. You'll need
+			// to adjust your project's target framework to 4.6 or higher
+			// (instructions for this are easily Googled).
+			// If you *need* to run on .NET 4.5 for compat/other reasons,
+			// the alternative is to 'return Task.Delay(0);' instead.
+			return Task.CompletedTask;
+		}
+	}
+}
