fedcffab5bd7197a84620ada59af1a2c9acf37e1
Mike Hughes <EdianCurrinir@gmail.com>
2019-03-22 15:22:32 -0400

    Added OteraDate Commands

--- BankerBot/Commands/Helper.cs
+++ BankerBot/Commands/Helper.cs
@@ -44,5 +44,38 @@ namespace BankerBot.Commands
             DateTime eastern = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Eastern Standard Time");
             await ReplyAsync("The current server time is: " + eastern.ToString("t"));
         }
+
+        [Command("OteraDate")]
+        public async Task OteraDate()
+        {
+            DateTime OteraDate = DateTime.Now.AddYears(-1019);
+            string DateString = OteraDate.ToLongDateString();
+
+            DateString = DateString.Replace("January", "Tressym");
+            DateString = DateString.Replace("February", "Auroch");
+            DateString = DateString.Replace("March", "Manticore");
+            DateString = DateString.Replace("April", "Almiraj");
+            DateString = DateString.Replace("May", "Dragon");
+            DateString = DateString.Replace("June", "Serpent");
+            DateString = DateString.Replace("July", "Unicorn");
+            DateString = DateString.Replace("August", "Hippogriff");
+            DateString = DateString.Replace("September", "Girallon");
+            DateString = DateString.Replace("October", "Cockatrice");
+            DateString = DateString.Replace("November", "Winter Wolf");
+            DateString = DateString.Replace("December", "Owlbear");
+
+            DateString = DateString.Replace("Monday, ", "");
+            DateString = DateString.Replace("Tuesday, ", "");
+            DateString = DateString.Replace("Wednesday, ", "");
+            DateString = DateString.Replace("Thursday, ", "");
+            DateString = DateString.Replace("Friday, ", "");
+            DateString = DateString.Replace("Saturday, ", "");
+            DateString = DateString.Replace("Sunday, ", "");
+            
+            DateString = DateString.Insert(DateString.IndexOf(',') + 1, " Swell - Ebb,");
+            DateString = DateString.Insert(DateString.Length, " A.G.");
+
+            await ReplyAsync("It is currently " + DateString);
+        }
 	}
 }
